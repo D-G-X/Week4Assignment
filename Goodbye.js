7cca7b23ed020c32177c59c23ee5f337e5164cb0
Darren <61945706+darren-rgx@users.noreply.github.com>
2020-05-30 12:22:48 +0530

    Add files via upload

---
+++ Goodbye.js
@@ -0,0 +1,7 @@
+greet="GoodBye ";
+(function (){
+	for (var i = namelist.length - 1; i >= 0; i--) {
+var name = namelist[i];
+ if (name[0]=="j"||name[0]=="J"){
+ 	console.log( greet + name);
+ }}})();
